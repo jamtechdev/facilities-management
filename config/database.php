3b1dd80fc5d1aac62da88093b4f0bed8dff0f0b3
jamtechdev <jamtechba@gmail.com>
2025-12-24 13:58:26 +0530

    fixed

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION') ?: (env('MYSQLHOST') || env('MYSQL_URL') ? 'mysql' : 'sqlite'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL') ?: env('MYSQL_URL') ?: env('MYSQL_PUBLIC_URL'),
-            'host' => env('DB_HOST') ?: env('MYSQLHOST', '127.0.0.1'),
-            'port' => env('DB_PORT') ?: env('MYSQLPORT', '3306'),
-            'database' => env('DB_DATABASE') ?: env('MYSQLDATABASE', 'laravel'),
-            'username' => env('DB_USERNAME') ?: env('MYSQLUSER', 'root'),
-            'password' => env('DB_PASSWORD') ?: env('MYSQLPASSWORD') ?: env('MYSQL_ROOT_PASSWORD', ''),
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
